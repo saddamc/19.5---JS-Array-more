e6288962ef1fc8808319ef016173228fdd88b115
saddamcall <saddam13bd@gmail.com>
2024-02-01 23:31:48 +0600

    first commit

---
+++ looping.js
@@ -0,0 +1,36 @@
+/*
+* LOOPING Technique
+* 1. for loop
+* 2. while loop
+* 3. do while --> ignore
+* 4. for of --> array loop korar jonno
+* 5. for in --> object loop korar jonno
+*/
+
+const friends = ['Elon', 'Bill', 'Mark', 'Waren']
+
+for(const friend of friends){
+    console.log(friend);
+}
+
+for(let i = 0; i < friends.length; i++){
+    console.log(i);
+    console.log(friends[i]);
+}
+
+const numbers = [5, 23, 456, 421, 556, 106, 320]
+
+for(let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}
+let i = 0;
+while(i < friends.length){
+    console.log(friends[i]);
+    i++;
+}
+
+let n = 0;
+while(n < numbers.length){
+    console.log(numbers[n]);
+    n++;
+}
